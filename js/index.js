727c3bad1dc9edc453e9d0ef043311ed6ec6a10d
Clarence-Coronel <clarence.coronel.r@gmail.com>
2023-12-27 08:31:25 +0800

    Add copy feature and view resume

--- js/index.js
+++ js/index.js
@@ -3,12 +3,10 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     listeners();
     goToSchoolLink();
+    copyText();
 })
-
-function copyToClipboard() {
-    // Get the text to copy
-    var textToCopy = document.querySelector("main").innerText;
-
+function copyToClipboard(textToCopy, type) {
+    let clicked = false;
     // Create a temporary textarea element
     var textarea = document.createElement("textarea");
 
@@ -27,8 +25,50 @@ function copyToClipboard() {
     // Remove the temporary textarea
     document.body.removeChild(textarea);
 
-    // Provide some visual feedback (optional)
-    alert("Text copied to clipboard: " + textToCopy);
+    if(type == "email"){
+        let ico = document.querySelector(`#copyEmail`);
+        let btn = document.querySelector(".copy-text[data-info='email']");
+        let text = document.querySelector(".email");
+
+        if(document.querySelector(`#copyPhone`).innerText == "done"){
+            document.querySelector(`#copyPhone`).innerText = "content_copy";
+            document.querySelector(".copy-text[data-info='phone']").classList.remove("copied");
+            document.querySelector(".phone-num").classList.remove("text-copied");
+        }
+
+        btn.classList.add("copied");
+        text.classList.add("text-copied")
+
+        ico.innerText = "done";
+
+        setTimeout(()=>{
+            ico.innerText = "content_copy";
+            btn.classList.remove("copied");
+            text.classList.remove("text-copied");
+        },5000)
+    }
+    else{
+        let ico = document.querySelector(`#copyPhone`);
+        let btn = document.querySelector(".copy-text[data-info='phone']");
+        let text = document.querySelector(".phone-num");
+
+        if(document.querySelector(`#copyEmail`).innerText == "done"){
+            document.querySelector(`#copyEmail`).innerText = "content_copy";
+            document.querySelector(".copy-text[data-info='email']").classList.remove("copied");
+            document.querySelector(".email").classList.remove("text-copied");
+        }
+
+        btn.classList.add("copied");
+
+        ico.innerText = "done";
+        text.classList.add("text-copied")
+
+        setTimeout(()=>{
+            ico.innerText = "content_copy";
+            btn.classList.remove("copied");
+            text.classList.remove("text-copied");
+        },5000)
+    }
 }
 
 function listeners(){
@@ -57,4 +97,15 @@ function goToSchoolLink(){
         })
         
     })
+}
+
+function copyText(){
+    let btns = document.querySelectorAll(".copy-text");
+
+    btns.forEach((btn)=>{
+        btn.addEventListener("click", ()=>{
+            if(btn.dataset.info == "email") copyToClipboard("clarence.coronel.r@gmail.com", "email");
+            else if(btn.dataset.info == "phone") copyToClipboard("0967 599 8955", "phone");
+        });
+    });
 }
