3c164a77d5c28c3c2ad6d9f498e06ce3a365ea55
Clarence-Coronel <clarence.coronel.r@gmail.com>
2023-12-31 21:04:54 +0800

    Fix back btn in archive opening new tab
    Instead of using button used anchor tag instead

--- js/archive.js
+++ js/archive.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     spotlightCursor()
-    addBackBtn();
 })
 
-function spotlightCursor(){
-    
+function spotlightCursor(){ 
 
     document.addEventListener('mousemove', function(event) {
         let light = document.getElementById('light');
@@ -26,13 +24,5 @@ function spotlightCursor(){
         // Update the position of the circular div
         light.style.left = mouseX - light.offsetWidth / 2 + 'px';
         light.style.top = mouseY - light.offsetHeight / 2 + 'px';
-    });
-}
-
-function addBackBtn(){
-    let btn = document.querySelector(".back-btn");
-
-    btn.addEventListener("click", ()=>{
-        window.open("./../index.html");
     });
 }
