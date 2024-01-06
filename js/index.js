e41909e4e3d664744be75954eb4f5aaf74d99120
Clarence-Coronel <clarence.coronel.r@gmail.com>
2024-01-06 19:54:58 +0800

    Add ischeduleadmin proj youtube link

--- js/index.js
+++ js/index.js
@@ -174,6 +174,9 @@ function mobileRedirectProj(proj){
     else if(proj == "nintendo"){
         window.open("https://youtu.be/BUBPs8jkx3Q", "_blank");
     }
+    else if(proj == "ischeduleadmin"){
+        window.open("https://youtu.be/pmj1Z1T57us", "_blank");
+    }
 }
 
 function copyToClipboard(textToCopy, type) {
@@ -301,6 +304,9 @@ function goToRepoLink(){
             }
             else if(project.dataset.proj == "nintendo"){
                 window.open("https://youtu.be/BUBPs8jkx3Q", "_blank");
+            }
+            else if(project.dataset.proj == "ischeduleadmin"){
+                window.open("https://youtu.be/pmj1Z1T57us", "_blank");
             }
         })
         
